2c77d574850d6c87f89a37f2e74b654e038ed46b
Mario <maalnu93@hotmail.com>
2023-10-30 09:35:04 +0100

    Empezando JS

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+//Autor: MArio ALfonso Nuñez (DAW)
+//GitHub:
+
+//Variables
+const PIN_CORRECTO = "6666"
+let chances = 3
+let saldo = "10000"
+
+//Enlazamos con el template
+
+depositarBtn = document.getElementById("dep")
+retirarBtn = document.getElementById("ret")
+transferirBtn = document.getElementById("trans")
+cambiarBtn = document.getElementById("change")
+salirBtn = document.getElementById("exit")
+
+
