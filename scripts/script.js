76356c942606348bb76e67eeec04febdf2226b20
Mario <maalnu93@hotmail.com>
2023-10-30 09:46:12 +0100

    Guardo por descanso

--- scripts/script.js
+++ scripts/script.js
@@ -13,5 +13,23 @@ retirarBtn = document.getElementById("ret")
 transferirBtn = document.getElementById("trans")
 cambiarBtn = document.getElementById("change")
 salirBtn = document.getElementById("exit")
+showSaldo = document.getElementById("saldo")
 
 
+
+function mostrarSaldo(){
+    showSaldo.innerText = saldo
+}
+
+function depositar(){
+    let monto
+    if(!isNaN())
+}
+
+
+mostrarSaldo()
+addEventListener.depositarBtn("click",depositar())
+addEventListener.retirarBtn("click",retirar())
+addEventListener.transferirBtn("click",transferir())
+addEventListener.cambiarBtn("click",cambiar())
+addEventListener.salirBtn("click",salir())
